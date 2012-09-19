012e8f3f4b5f804c3437fd61e2f270b027b127b3
Jeff Graham <jeff@funnymonkey.com>
2012-09-19 12:34:16 -0700

    Updating license text

--- FunnyMonkey/EPUB/EPUBPackage.php
+++ FunnyMonkey/EPUB/EPUBPackage.php
@@ -7,20 +7,20 @@
  * @author    Jeff Graham
  * @version   version 0.1
  * @date      2012
- * @copyright http://www.gnu.org/licenses/agpl-3.0.html
- *   Copyright (C) 2012  Jeff Graham
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Affero General Public License as
- *   published by the Free Software Foundation, either version 3 of the
- *   License, or (at your option) any later version.
+ * @copyright Copyright (C) 2012 Jeff Graham
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @todo 3.4.15. The bindings Element
  * @see http://idpf.org/epub/30/spec/epub30-publications.html#sec-bindings-elem
